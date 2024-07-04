9ffb0adbb4dd6ecbad2e3e3797b1fb81aa7c7e7e
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2024-07-04 16:52:38 -0300

    finished day 1

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,13 @@
+import { PopoverButton } from "@headlessui/react";
+import { X } from "phosphor-react";
+
+export function CloseButton(){
+  return(
+    <PopoverButton
+      className='top-5 right-5 absolute text-zinc-400 hover:text-zinc-100'
+      title="Fechar formulário de feedback"
+    >
+      <X weight="bold" className="w-4 h-4"/>
+    </PopoverButton>
+  )
+}
---
+++ src/components/Widget.tsx
@@ -0,0 +1,28 @@
+import { ChatTeardropDots } from "phosphor-react";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { WidgetForm } from "./WidgetForm/index";
+
+export function Widget(){
+
+  return (
+    <Popover className="absolute bottom-4 right-4 md:bottom-10 md:right-10 fkex-col items-end">
+      <PopoverPanel>
+        <WidgetForm/>
+      </PopoverPanel>
+
+      <PopoverButton 
+        className="flex items-center bg-brand-500 rounded-full px-3 h-12 text-white group"
+      >
+        <ChatTeardropDots className="w-6 h-6 "/>
+        <span 
+          className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear"
+        >
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </PopoverButton>
+
+    </Popover>
+    
+  )
+}
